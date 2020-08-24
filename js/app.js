b59c7af40375fcd139a0b2ca8f541ca9d942d2bf
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-08-24 07:30:05 +0100

    weekend hw

---
+++ js/app.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const newCarForm = document.querySelector('#new-car-form');
+  newCarForm.addEventListener('submit', handleNewCarForm);
+
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
+
+});
+
+const handleNewCarForm = function (event) {
+  event.preventDefault();
+
+  const carDetailListItem = createCarDetailListItem(event.target);
+  const carDetail = document.querySelector('#car-detail');
+  carDetail.appendChild(carDetailListItem);
+
+  event.target.reset();
+};
+
+  const createCarDetailListItem = function (form) {
+    const carDetailListItem  = document.createElement('li');
+    carDetailListItem.classList.add('car-Details-List-Item');
+
+    const make = document.createElement('h4');
+    make.textContent = form.make.value;
+    carDetailListItem.appendChild(make);
+
+    const model = document.createElement('h4');
+    model.textContent = form.model.value;
+    carDetailListItem.appendChild(model);
+
+    const country = document.createElement('h4');
+    country.textContent = form.country.value;
+    carDetailListItem.appendChild(country);
+
+    const body = document.createElement('h4');
+    body.textContent = form.body.value;
+    carDetailListItem.appendChild(body);
+
+    const fuel = document.createElement('h4');
+    fuel.textContent = form.fuel.value;
+    carDetailListItem.appendChild(fuel);
+
+    const gear = document.createElement('h4');
+    gear.textContent = form.gear.value;
+    carDetailListItem.appendChild(gear);
+
+    const engine = document.createElement('h4');
+    engine.textContent = form.engine.value;
+    carDetailListItem.appendChild(engine);
+
+    return carDetailListItem;
+  };
+
+  const handleDeleteAllClick = function (event) {
+  const carDetail = document.querySelector('#car-detail');
+  carDetail.innerHTML = "";
+};
